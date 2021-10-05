fa688e917765af8453403456979bb45892283e17
Soronikasarker6 <soronika.sarker.cse@gmail.com>
2021-10-05 10:27:48 +0600

    Initial commit

---
+++ app/src/main/java/com/ex1/myapp/Chat_room.java
@@ -0,0 +1,14 @@
+package com.ex1.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chat_room extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
+}
